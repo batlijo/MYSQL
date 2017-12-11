e407e0236a81092be8224e8e7b2e33f21ee4638d
Batli Joselevitz <batlijoselevitz@andreas-MBP.localdomain>
2017-12-11 14:45:03 -0600

    added notes

--- mysqlday3.js
+++ mysqlday3.js
@@ -48,3 +48,49 @@ adding an index into workbench
 */
 
 alter table top5000 add index (artist)
+
+
+/* THIS IS PRACTICE AND USEFUL INFO FROM PGEXERCISES]
+
+
+QUESTION:
+How can you produce a list of facilities that charge a fee to members,
+and that fee is less than 1/50th of the monthly maintenance cost?
+Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
+
+ANSWER:
+select * from cd.facilities where membercost > 0;
+
+QUESTION:
+How can you produce a list of all facilities with the word 'Tennis' in their name?
+
+ANSWER:
+select *
+	from cd.facilities
+	where
+		name like '%Tennis%';
+
+QUESTION:
+How can you produce a list of facilities,
+with each labelled as 'cheap' or 'expensive' depending
+on if their monthly maintenance cost is more than $100?
+Return the name and monthly maintenance of the facilities in question.
+
+ANSWER: select name,
+	case when (monthlymaintenance > 100) then
+		'expensive'
+	else
+		'cheap'
+	end as cost
+	from cd.facilities;
+
+  QUESTION:
+  How can you produce a list of members who joined after the start of September 2012?
+  Return the memid, surname, firstname, and joindate of the members in question.
+
+  ANSWER:
+  select memid, surname, firstname, joindate
+	from cd.members
+	where joindate >= '2012-09-01';  
+
+*/
